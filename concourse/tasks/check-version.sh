b8f2ee1dcb968a8b8699bdf8d366ca4307d04e25
ArthurHlt <arthur.halet@orange.com>
2017-11-02 20:07:13 +0100

    add more slack message in concourse releasing

--- concourse/tasks/check-version.sh
+++ concourse/tasks/check-version.sh
@@ -4,6 +4,18 @@ set -xe
 
 CW=$(pwd)
 
+send_slack() {
+    if [ "x$slack_url" == "x" ]; then
+        return 0
+    fi
+    message=$1
+    insecure=""
+    if [ "x$slack_insecure" != "x" ]; then
+        insecure="--insecure"
+    fi
+    notifslack $insecure --url $slack_url -c $slack_channel -u $slack_username -i $slack_icon "$message"
+}
+
 actual_version=$(curl -s http://about.mattermost.com/download/ -L | pup '.p1.s1' | grep Release | awk '{print $3}')
 current_version=$(head -n 1 "$CW/mattermost-integrator-release/tag")
 if [ "v$actual_version" == "$current_version" ]; then
@@ -16,16 +28,10 @@ echo "$semver" | grep -Eq "^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}$"
 if [ $? -ne 0 ]; then
     message="[mattermost] Parsed version do not follow semantic versioning, check https://www.mattermost.org/download page and fix the parsing"
     echo "$message"
-    if [ "x$slack_url" == "x" ]; then
-        exit 1
-    fi
-    insecure=""
-    if [ "x$slack_insecure" != "x" ]; then
-        insecure="--insecure"
-    fi
-    notifslack $insecure --url $slack_url -c $slack_channel -u $slack_username -i $slack_icon "$message"
+    send_slack "$message"
     exit 1
 fi
+send_slack "Mattermost-cf-integrator bump its version to $semver"
 echo "$semver" > "$CW/release-info/tag_to_release"
 echo "Mattermost $semver on Cloud Foundry" > "$CW/release-info/name_of_release"
 echo "See changelog from mattermost: https://docs.mattermost.com/administration/changelog.html#release-v$semver" > "$CW/release-info/body"
